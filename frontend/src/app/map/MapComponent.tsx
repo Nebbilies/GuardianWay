ece9f2c398e5c391337b54ecc9fec0f448dd9693
nebbilies <hirohamada17092005@gmail.com>
2026-03-24 10:00:16 +0700

    feat: add a map simulation page

---
+++ frontend/src/app/map/MapComponent.tsx
@@ -0,0 +1,46 @@
+'use client'
+import {MapContainer, Marker, Popup, TileLayer, useMapEvents} from "react-leaflet";
+import {RefObject, useRef, useState} from "react";
+import L, {latLng, LatLng} from "leaflet";
+
+interface MapDescendantProps {
+    onMapClick: (position: { lat: number, lng: number }) => void;
+}
+
+function MapDescendant({ onMapClick }: MapDescendantProps) {
+    const map = useMapEvents({
+        click(e) {
+            onMapClick(e.latlng);
+            map.flyTo(e.latlng, 17, {
+                duration: 1.2
+            })
+        }
+    })
+    return null;
+}
+
+interface MapComponentProps {
+    latLng: { lat: number, lng: number } | null;
+    setLatLng: (position: { lat: number, lng: number }) => void;
+}
+
+export default function MapComponent({ latLng, setLatLng }: MapComponentProps) {
+    const mapRef: RefObject<L.Map | null> = useRef<L.Map | null>(null);
+    return (
+            <MapContainer center={[10.832, 106.6297]} zoom={13} scrollWheelZoom={true}
+                            style={{ height: "600px", width: "100%" }} ref={mapRef}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <MapDescendant onMapClick={setLatLng}/>
+                {latLng && (
+                    <Marker position={latLng}>
+                        <Popup>
+                            Selected Location
+                        </Popup>
+                    </Marker>
+                )}
+            </MapContainer>
+    )
+}
---
+++ frontend/src/app/map/page.tsx
@@ -0,0 +1,33 @@
+'use client'
+import dynamic from 'next/dynamic'
+import { useState } from "react";
+
+const MapComponent = dynamic(() => import('./MapComponent'), {
+    ssr: false,
+});
+
+export default function MapPage() {
+    const [latLng, setLatLng] = useState<{ lat: number, lng: number } | null>(null);
+
+    return (
+        <main className="min-h-screen bg-background flex flex-col items-center justify-center w-full p-4">
+            <div className="relative flex flex-col bg-card rounded-xl shadow-md border border-border overflow-hidden w-2/3 items-center">
+                <div className="h-full w-full z-0 flex items-center justify-center">
+                    <MapComponent latLng={latLng} setLatLng={setLatLng} />
+                </div>
+                <div className="p-5 flex items-center justify-center bg-secondary/10 border-t border-border w-2/3">
+                    {latLng ? (
+                        <p className="text-lg font-medium text-foreground">
+                            Tọa độ đã chọn: <span className="font-mono text-primary font-bold">{latLng.lat.toFixed(5)}, {latLng.lng.toFixed(5)}</span>
+                        </p>
+                    ) : (
+                        <p className="w-full text-lg text-center text-muted-foreground italic p-4">
+                            Chưa có tọa độ.
+                        </p>
+                    )}
+                </div>
+
+            </div>
+        </main>
+    )
+}
