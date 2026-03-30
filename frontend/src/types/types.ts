7f5b2eeba80d8a3c92c135334a5e14f0b720e335
nebbilies <hirohamada17092005@gmail.com>
2026-03-30 15:30:11 +0700

    feat: add types for backend models

---
+++ frontend/src/types/types.ts
@@ -0,0 +1,3 @@
+import {User, StudentProfile, DriverProfile, Bus, BusStop, BusRoute, RouteStop, BusTrip, BoardingRecord, TrackingLog, Notification } from "@gw/backend"
+
+export type { User, StudentProfile, DriverProfile, Bus, BusStop, BusRoute, RouteStop, BusTrip, BoardingRecord, TrackingLog, Notification }
