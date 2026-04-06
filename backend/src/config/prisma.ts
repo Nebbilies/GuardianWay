3d7678cfaf88eb5453c0ea585769747808be0a12
nebbilies <hirohamada17092005@gmail.com>
2026-04-06 14:25:51 +0700

    feat: implement pagination for fetching bus stops with search and sorting options

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,9 +1,19 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 import "dotenv/config";
+import {pagination} from "prisma-extension-pagination";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({ adapter }).$extends(pagination({
+    pages: {
+        limit: 10,
+        includePageCount: true,
+    },
+    cursor: {
+        limit: 10,
+        includeTotalCount: true,
+    }
+}));
 
 export default prisma;
 export * from "@prisma/client"
--- backend/src/repositories/busStop.repository.ts
+++ backend/src/repositories/busStop.repository.ts
@@ -1,9 +1,38 @@
 import prisma from "../config/prisma";
 import {BusStop} from "@prisma/client";
+import {PaginatedResult} from "@gw/shared"
+
+interface GetAllBusStopsParams {
+    search?: string;
+    isSchoolStop?: boolean;
+    page?: number;
+    limit?: number;
+    sort?: string;
+}
 
 class BusStopRepository {
-    async getAll(): Promise<BusStop[]> {
-        return prisma.busStop.findMany();
+    async getAll(params: GetAllBusStopsParams = {}): Promise<PaginatedResult<BusStop>> {
+        const sortParam = params?.sort || 'name';
+        const isDesc = sortParam.startsWith('-');
+        const sortBy = isDesc ? sortParam.substring(1) : sortParam;
+        const sortOrder = isDesc ? 'desc' : 'asc';
+
+        const [data, metadata] = await prisma.busStop.paginate({
+            where: {
+                name: params.search ? { contains: params.search, mode: 'insensitive' } : undefined,
+                isSchoolStop: params.isSchoolStop,
+            },
+            orderBy: { [sortBy]: sortOrder },
+        }).withPages({
+            page: params?.page || 1,
+            limit: params?.limit || 10,
+            includePageCount: true,
+        });
+
+        return {
+            data,
+            metadata,
+        };
     }
 
     async create(data: Pick<BusStop, "name" | "address" | "latitude" | "longitude" | "isSchoolStop">): Promise<BusStop> {
