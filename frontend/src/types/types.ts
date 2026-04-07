db0d9b6be23a427c88133ba590162e65e1ac3877
nebbilies <hirohamada17092005@gmail.com>
2026-04-07 11:21:40 +0700

    chore: import types from shared to frontend

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,3 +1,5 @@
 import {User, StudentProfile, DriverProfile, Bus, BusStop, BusRoute, RouteStop, BusTrip, BoardingRecord, TrackingLog, Notification } from "@gw/backend"
+import { PaginationMetadata, PaginatedResponse } from "@gw/shared"
 
-export type { User, StudentProfile, DriverProfile, Bus, BusStop, BusRoute, RouteStop, BusTrip, BoardingRecord, TrackingLog, Notification }
+export type { User, StudentProfile, DriverProfile, Bus, BusStop, BusRoute, RouteStop,
+    BusTrip, BoardingRecord, TrackingLog, Notification, PaginatedResponse, PaginationMetadata }
