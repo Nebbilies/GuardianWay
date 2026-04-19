0f7de79a86571054fd754d1f94c4db88633b152e
nebbilies <hirohamada17092005@gmail.com>
2026-04-19 11:36:11 +0700

    feat(user): add user API routes

---
+++ backend/src/routes/user.routes.ts
@@ -0,0 +1,13 @@
+import {Router} from "express";
+import {userController} from "../controllers/user.controller";
+
+const router = Router();
+
+router.get("/parents", userController.getParents);
+router.get("/", userController.getAll);
+router.get("/:id", userController.getById);
+router.post("/", userController.create);
+router.put("/:id", userController.edit);
+router.delete("/:id", userController.delete);
+
+export default router;
