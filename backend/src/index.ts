e0a1cf81821ee5bb4c18cf09d53ef39007662dbd
nebbilies <hirohamada17092005@gmail.com>
2026-04-22 09:48:17 +0700

    feat(busTrip): update bus trip routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import busStopRoutes from "./routes/busStop.routes";
 import busRouteRoutes from "./routes/busRoute.routes";
 import busRoutes from "./routes/bus.routes";
 import userRoutes from "./routes/user.routes";
+import busTripRoutes from "./routes/busTrip.routes";
 export { default as prisma } from "./config/prisma";
 export * from "./config/prisma";
 
@@ -15,6 +16,7 @@ app.use("/api/bus-stops", busStopRoutes);
 app.use("/api/bus-routes", busRouteRoutes);
 app.use("/api/buses", busRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/bus-trips", busTripRoutes);
 app.get("/", (req: Request, res: any) => {
     res.send("Hello, api is on!");
 })
