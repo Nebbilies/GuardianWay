296de3bf25b1e0e68765e7facb2f363db24d3918
nebbilies <hirohamada17092005@gmail.com>
2026-04-22 09:37:41 +0700

    feat(types): add BusTripWithDetails type

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,5 +1,18 @@
 import { Prisma } from "@gw/backend"
-import {User, StudentProfile, DriverProfile, Bus, BusStop, BusRoute, RouteStop, BusTrip, BoardingRecord, TrackingLog, Notification } from "@gw/backend"
+import {
+    User,
+    StudentProfile,
+    DriverProfile,
+    Bus,
+    BusStop,
+    BusRoute,
+    RouteStop,
+    BusTrip,
+    BoardingRecord,
+    TrackingLog,
+    Notification,
+    BusTripStatus
+} from "@gw/backend"
 import { PaginationMetadata, PaginatedResponse } from "@gw/shared"
 
 type BusRouteWithStops = Prisma.BusRouteGetPayload<{
@@ -19,5 +32,19 @@ type UserWithProfiles = Prisma.UserGetPayload<{
     }
 }>
 
+type BusTripWithDetails = Prisma.BusTripGetPayload<{
+    include: {
+        route: true;
+        bus: true;
+        driver: {
+            include: {
+                user: true;
+            }
+        }
+    }
+}>
+
 export type { User, StudentProfile, DriverProfile, Bus, BusStop, BusRoute, BusRouteWithStops, RouteStop,
-    BusTrip, BoardingRecord, TrackingLog, Notification, PaginatedResponse, PaginationMetadata, UserWithProfiles }
+    BusTrip, BoardingRecord, TrackingLog, Notification, PaginatedResponse, PaginationMetadata, UserWithProfiles,
+    BusTripWithDetails, BusTripStatus
+}
